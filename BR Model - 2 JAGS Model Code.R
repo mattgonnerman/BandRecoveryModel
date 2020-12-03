65d6834f7313fa1a9d09638bd75656853002ed7d
Matt Gonnerman <matthew.gonnerman@maine.edu>
2020-12-03 11:49:15 -0500

    Broke Up Code for easier viewing

---
+++ BR Model - 2 JAGS Model Code.R
@@ -0,0 +1,215 @@
+function(){##############################################################################################
+  ### Weekly Survival Rate ###
+  alpha_s ~ dbeta(1,1)
+  intercept_s <- logit(alpha_s)
+  beta_F_s ~ dunif(0,1) #Effect of sex on WSR (Male reference)
+  beta_A_s ~ dunif(0,1) #Effect of age on WSR (Juv reference)
+  beta_A_F_s ~ dunif(0,1) #Interaction term for Age/Sex(Male Juv reference)
+  beta_S2W_s ~ dunif(0,1) #Effect of S2W (W2S reference)
+  for(i in sampledwmd){beta_wmd_s[i] ~ dunif(0,1)} #Effect of wmd (W2S reference)
+  
+  #WSR
+  for(i in 1:nvisit){
+    eta[i] <- intercept_s + 
+      beta_F_s*wsr_sex[i] + beta_A_s*wsr_age[i] + beta_A_F_s*wsr_age[i]*wsr_sex[i] + 
+      beta_S2W_s*wsr_time[i] + beta_wmd_s[wsr_wmd[i]]
+    logit(phi[i])<-eta[id[i]] # anti-logit to determine the daily survival rate
+    mu[i]<-pow(phi[i],interval[i]) # period survival is DSR raised to the interval
+    succ[i]~dbern(mu[i])  # the data is distributed as bernoulli with period survival as the mean
+  }
+  
+  ##############################################################################################
+  ### Band Recovery ###
+  alpha_hr ~ dbeta(1,1)
+  intercept_hr <- logit(alpha_hr)
+  beta_A_hr ~ dunif(0,1) #Effect of Age on band recovery (Juv reference)
+  beta_2019_hr ~ dunif(0,1) #Effect of Year on band recovery (2019 vs other) #This could probably be coded as a loop in the future to make it easier for IFW
+  beta_2020_hr ~ dunif(0,1) #Effect of Year on band recovery (2020 vs other)
+  beta_spp ~ dnorm(0, 1)
+  
+  #Specify period specific survival/band recovery
+  for(j in 1:nind){
+    for(t in f[j]:n.occasions){
+      #Survival Model
+      logit(s.br[j,t]) <- intercept_s + 
+        beta_A_s*br_age[j,t] + beta_S2W_s*br_s2w[j,t] + beta_wmd_s[br_wmd[j]]
+      #Band Recovery Model
+      logit(hr.br[j,t]) <- intercept_hr + 
+        beta_A_hr*br_age[j,t] + beta_2019_hr*br_2019[j,t] + beta_2020_hr*br_2020[j,t] + 
+        w.tilde[cap.site[j]] + e.cap[cap.site[j]]
+      
+      s[j,t] <- ifelse(t == f[j], pow(s.br[j,t], weeks2harv[j]), 
+                       ifelse(t == 1 || t == 3 || t == 5, pow(s.br[j,t], 11), pow(s.br[j,t], 36)))
+      hr[j,t] <- ifelse(t == 1 || t == 3 || t == 5, 0, hr.br[j,t])
+      
+    } #t
+  } #i
+  
+  # Residual error (w/ corrections)
+  for(i in 1:N.cap){
+    e.cap[i] ~ dnorm(0, prec[i])
+    prec[i] <- 1/var.all[i]
+    var.all[i] <- tausq + sigmasq[1] - correction[i] 
+  }
+  
+  ##Process##
+  for(k in 1:nind){
+    w[k,f[k]]<-1 #define true survival state at first capture
+    z[k,f[k]]<-1 #define availability for natural risk at first capture
+    
+    for(t in (f[k]+1):n.occasions){ #Starts first occasion post capture
+      z[k,t] ~ dbern(mu1[k,t]) #State process (Does bird survive t-1 to t, natural risk)
+      mu1[k,t] <- s[k,t-1]*w[k,t-1] #Probability that a bird survives t-1 to t
+      
+      y[k,t] ~ dbern(mu2[k,t]) #Observation process (Is bird shot in t)
+      mu2[k,t] <- hr[k,t]*z[k,t] #Probability of harvest * if bird was alive
+      
+      w[k,t] <- z[k,t]-y[k,t] #True latent survival state
+    } #t
+  } #k
+  
+  ##############################################################################################
+  ### Spatial Predictive Process ###
+  sigmasq <- 1/sigmasq.inv
+  sigmasq.inv ~ dgamma(2,1)
+  phi.spp ~ dgamma(1,0.1)
+  tausq <- 1/tausq.inv
+  tausq.inv ~ dgamma(0.1,0.1)
+  
+  w.tilde.star[1:N.knot] ~ dmnorm(mu.w.star[1:N.knot], C.star.inv[1:N.knot,1:N.knot])
+  C.star.inv[1:N.knot,1:N.knot] = inverse(C.star[1:N.knot,1:N.knot])
+  
+  for (i in 1:N.knot) {
+    mu.w.star[i] = 0
+    C.star[i,i] = sigmasq
+    for (j in 1:(i-1)) {
+      C.star[i,j] = sigmasq*exp(-(d.s.star.star[i,j]/phi.spp))
+      C.star[j,i] = C.star[i,j]
+    } }
+  # Interpolate spatial PP back on to original sites
+  for(i in 1:N.cap) {
+    for(j in 1:N.knot) {
+      C.s.star[i,j] = sigmasq*exp(-(d.s.star[i,j]/phi.spp))
+    } }
+  w.tilde[1:N.cap] = C.s.star[1:N.cap,1:N.knot]%*%C.star.inv[1:N.knot,1:N.knot]%*%w.tilde.star[1:N.knot]
+  #Variance Correction
+  for(i in 1:N.cap){
+    correction[i] = t(C.s.star[i,1:N.knot])%*%C.star.inv[1:N.knot,1:N.knot]%*%C.s.star[i,1:N.knot]
+  }
+  
+  ##############################################################################################
+  ### Derived Parameters ###
+  #Capture Site specific harvest rates
+  for(i in 1:N.cap){
+    logit(HR.A.2019.cap[i]) <- intercept_hr + beta_A_hr + beta_2019_hr + w.tilde[i] + e.cap[i]
+    logit(HR.J.2019.cap[i]) <- intercept_hr + beta_2019_hr + w.tilde[i] + e.cap[i]
+  }
+  
+  #Knot specific harvest rates
+  for(i in 1:N.knot){
+    logit(HR.A.2019.knot[i]) <- intercept_hr + beta_A_hr + beta_2019_hr + w.tilde.star[i]
+    logit(HR.J.2019.knot[i]) <- intercept_hr + beta_2019_hr + w.tilde.star[i]
+  }
+  
+  ### WMD Specific Harvest Rates
+  for(i in 1:N.wmd){
+    mean.WMD.HR.J[WMD.id[i]] <- mean(HR.J.2019.knot[WMD.matrix[i, 1:WMD.vec[i]]])
+    mean.WMD.HR.A[WMD.id[i]] <- mean(HR.A.2019.knot[WMD.matrix[i, 1:WMD.vec[i]]])
+  }
+  
+  ### Period Specific Survival
+  for(i in sampledwmd){
+    logit(WSR_M_J_S2W[i]) <- intercept_s + beta_S2W_s + beta_wmd_s[i]
+    logit(WSR_M_A_S2W[i]) <- intercept_s + beta_A_s + beta_S2W_s + beta_wmd_s[i]
+    logit(WSR_M_J_W2S[i]) <- intercept_s + beta_wmd_s[i]
+    logit(WSR_M_A_W2S[i]) <- intercept_s + beta_A_s + beta_wmd_s[i]
+  }
+  
+  #Averaged Period Specific Survival for all WMDs sampled
+  S_M_J_S2W ~ dnorm(pow(mean(WSR_M_J_S2W[sampledwmd]), 36), .05)
+  S_M_A_S2W ~ dnorm(pow(mean(WSR_M_A_S2W[sampledwmd]), 36), .05)
+  S_M_J_W2S ~ dnorm(pow(mean(WSR_M_J_W2S[sampledwmd]), 11), .05)
+  S_M_A_W2S ~ dnorm(pow(mean(WSR_M_A_W2S[sampledwmd]), 11), .05)
+  
+  #Average Non Harvest Survival by WMD
+  mean.AnnualS.A <- S_M_A_W2S * S_M_A_S2W
+  mean.AnnualS.J <- S_M_J_W2S * S_M_J_S2W
+  
+  ##############################################################################################
+  ### State-Space Abundance ###
+  #Total Harvest Observation Error
+  tau.obs.A <- pow(sigma.obs.A, -2)
+  sigma2.obs.A <- pow(sigma.obs.A, 2)
+  sigma.obs.A ~ dunif(0,100)
+  tau.obs.J <- pow(sigma.obs.J, -2)
+  sigma2.obs.J <- pow(sigma.obs.J, 2)
+  sigma.obs.J ~ dunif(0,100)
+  
+  #Recruitment Process Error
+  tau.R <- pow(sigma.R, -2)
+  sigma2.R <- pow(sigma.R, 2)
+  sigma.R ~ dunif(0,10)
+  
+  #Survival Annual Process Error
+  tau.surv.A <- pow(sigma.surv.A, -2)
+  sigma2.surv.A <- pow(sigma.surv.A, 2)
+  sigma.surv.A ~ dunif(0,1)
+  tau.surv.J <- pow(sigma.surv.J, -2)
+  sigma2.surv.J <- pow(sigma.surv.J, 2)
+  sigma.surv.J ~ dunif(0,1)
+  
+  #Harvest Rate Annual Process Error
+  tau.harv.A <- pow(sigma.harv.A, -2)
+  sigma2.harv.A <- pow(sigma.harv.A, 2)
+  sigma.harv.A ~ dunif(0,1)
+  tau.harv.J <- pow(sigma.harv.J, -2)
+  sigma2.harv.J <- pow(sigma.harv.J, 2)
+  sigma.harv.J ~ dunif(0,1)
+  
+  for(i in 1:N.wmd){
+    for(t in 1:n.years){
+      #Total harvest Observation
+      th.A[WMD.id[i],t] ~ dnorm(totharv.A[WMD.id[i],t], tau.obs.A)
+      th.J[WMD.id[i],t] ~ dnorm(totharv.J[WMD.id[i],t], tau.obs.J)
+      
+      #Total harvested = Harvest Rate * Total Abundance
+      totharv.A[WMD.id[i],t] <- N.A[WMD.id[i],t]*WMD.HR.A[WMD.id[i],t]
+      totharv.J[WMD.id[i],t] <- N.J[WMD.id[i],t]*WMD.HR.J[WMD.id[i],t]
+      
+      #Annual Variation in Harvest Rate
+      WMD.HR.A[WMD.id[i],t] ~ dnorm(mean.WMD.HR.A[WMD.id[i]], tau.harv.A)
+      WMD.HR.J[WMD.id[i],t] ~ dnorm(mean.WMD.HR.J[WMD.id[i]], tau.harv.J)
+    }
+    
+    #Need to have specify N[t=1], needs to be a whole number.
+    #th.year1 are just the harvest totals from year 1 
+    N.A[WMD.id[i],1] <- round(((1+th.year1.A[WMD.id[i]])/mean.WMD.HR.A[WMD.id[i]]))
+    N.J[WMD.id[i],1] <- round(((1+th.year1.J[WMD.id[i]])/mean.WMD.HR.J[WMD.id[i]]))
+    
+    #Average Recruitment Rate, WMD specific
+    # mean.R[WMD.id[i]] ~ dunif(0,2)
+    mean.R[WMD.id[i]] <- .5
+    
+    for(t in 1:(n.years-1)){
+      #Number of birds to A to surive OR J that transition into A from t to t+1
+      N.A[WMD.id[i],t+1] <- n.surv.A[WMD.id[i],t] + n.surv.J[WMD.id[i],t]
+      n.surv.A[WMD.id[i],t] ~ dbin(totalS.A[WMD.id[i],t], N.A[WMD.id[i],t])
+      n.surv.J[WMD.id[i],t] ~ dbin(totalS.J[WMD.id[i],t], N.J[WMD.id[i],t])
+      
+      #Total Annual Survival Probability
+      totalS.A[WMD.id[i],t] <- AnnualS.A[WMD.id[i],t]*(1-WMD.HR.A[WMD.id[i],t])
+      totalS.J[WMD.id[i],t] <- AnnualS.J[WMD.id[i],t]*(1-WMD.HR.J[WMD.id[i],t])
+      
+      #Temporal Variation in probability of surviving non harvest risk in a year
+      AnnualS.A[WMD.id[i],t] ~ dnorm(mean.AnnualS.A, tau.surv.A)
+      AnnualS.J[WMD.id[i],t] ~ dnorm(mean.AnnualS.J, tau.surv.J)
+      
+      #Number of Birds recruited to the Juvenile population in t
+      N.J[WMD.id[i],t+1] ~ dpois(meanY1[WMD.id[i],t])
+      meanY1[WMD.id[i],t] <- R[WMD.id[i],t] * (N.A[WMD.id[i],t] + N.J[WMD.id[i],t])
+      
+      #Year Specific recruitment rate
+      R[WMD.id[i],t] ~ dnorm(mean.R[WMD.id[i]], tau.R)
+    }
+  }
+}
---
+++ BR Model - 3 Execute JAGS.R
@@ -0,0 +1,124 @@
+###################################################################################################################
+###################################################################################################################
+###################################################################################################################
+#######################
+### Models for JAGS ###
+#######################
+### Data needed for MCMC ###
+# Succ: vector of whether a nest survived an interval (1) or not (0)
+# Interval: vector of interval lengths that corresponds with each value in succ
+# X: matrix of covariates
+# id: vector of nest IDs
+
+dat <- list( succ = succ, #Adult Survival
+             interval = interval, #Adult Survival
+             nvisit = length(succ), #Adult Survival
+             id = ID, #Adult Survival
+             wsr_sex = wsr_sex, #Adult Survival
+             wsr_age = wsr_age, #Adult Survival
+             wsr_time = wsr_time, #Adult Survival
+             wsr_wmd = wsr_wmd,
+             y = EH_raw, #Band Recovery
+             f = f, #Band Recovery
+             weeks2harv = weeks2harv,#BandRecovery
+             nind = dim(EH_raw)[1], #Band Recovery 
+             n.occasions = dim(EH_raw)[2], #Band Recovery 
+             z = known.state.mr(EH_raw), #Band Recovery
+             br_age = br_age, #Band Recovery
+             br_2019 = br_2019, #Band Recovery
+             br_2020 = br_2020, #Band Recovery
+             br_s2w = br_s2w, #Band Recovery
+             br_wmd = br_wmd,
+             th.A = totharv.A, #Total Adult Harvest by WMD '14-'19
+             th.J = totharv.J, #Total Juvenile Harvest by WMD '14-'19 
+             th.year1.A = as.integer(totharv.A[,1]), #Total Adult Harvest by WMD '14-'19
+             th.year1.J = as.integer(totharv.J[,1]), #Total Juvenile Harvest by WMD '14-'19
+             n.years = ncol(totharv.A),
+             sampledwmd = sampledwmd, #list of wmd's where we sampled
+             cap.site = ind.cap.site, #each individuals capture site as a numeric
+             d.s.star=KnotLocalDis.mat/1000, #distance between spatial knots and cap sites
+             d.s.star.star=KnotDis.mat/1000, #distance between spatial knots and other spatial knots
+             N.cap = length(unique(ind.cap.site)), #number of capture sites for SPP
+             N.knot = nrow(KnotDis.mat), #number of knots for SPP
+             N.wmd = nrow(WMD.matrix), #number of WMDs we are using in SPP
+             WMD.matrix = WMD.matrix, #matrix showing which wmd a knot is in
+             WMD.vec = WMD.vec, #vector for referencing WMD.matrix
+             WMD.id = sort(WMD.id) #vector to ID WMDs in SPP
+) #for Harvest rate estimates
+
+#Parameters monitors
+parameters.null <- c('alpha_s', 
+                     'alpha_hr', 
+                     'intercept_s', #Non-Harvest Survival Intercept 
+                     'beta_F_s', #Non-Harvest Survival Beta - Female
+                     'beta_A_s', #Non-Harvest Survival Beta - Adult
+                     'beta_A_F_s', #Non-Harvest Survival Beta - F*A Interaction
+                     'beta_S2W_s', #Non-Harvest Survival Beta - Spring to Winter period
+                     'beta_wmd_s', #Non-Harvest Survival Beta - WMD specific
+                     'intercept_hr', #Harvest Rate Intercept
+                     'beta_A_hr', #Harvest Rate Betas - Adult
+                     'beta_2019_hr', #Harvest Rate Betas - 2019
+                     'beta_2020_hr', #Harvest Rate Betas - 2020
+                     'w.tilde', 
+                     'w.tilde.star',
+                     'phi.spp', 
+                     'sigmasq',
+                     'tausq',
+                     'HR.A.2019.knot',
+                     'HR.J.2019.knot',
+                     'HR.A.2019.cap',
+                     'HR.J.2019.cap',
+                     'mean.WMD.HR.A',
+                     'mean.WMD.HR.J',
+                     'sigma2.harv.A',
+                     'sigma2.harv.J',
+                     'S_M_J_W2S', #Juvenile Survival Capture to Start of Hunting Season
+                     'S_M_A_W2S', #Juvenile Survival Capture to Start of Hunting Season
+                     'S_M_J_S2W', #Juvenile Survival Capture to Start of Hunting Season
+                     'S_M_A_S2W', #Juvenile Survival Capture to Start of Hunting Season
+                     'N.A',
+                     'N.J',
+                     'mean.R',
+                     'sigma2.R',
+                     'mean.AnnualS.J',
+                     'mean.AnnualS.A',
+                     'sigma2.surv.J',
+                     'sigma2.surv.A'
+)
+
+#Initial values
+inits.null <- function(){
+  list(z = mr.init.z(EH_raw))
+}
+
+#MCMC settings
+ni <- 2000 #number of iterations
+nt <- 8 #thinning
+nb <- 1000 #burn in period
+nc <- 3 #number of chains
+
+#Model for JAGS
+
+br_w_as_model <- source(file = "BR Model - 2 JAGS Model Code.R")$value
+
+
+### Run Model ###
+#Call JAGS
+BR_w_SPP_output <- jags(data = dat,
+                        parameters.to.save = parameters.null,
+                        inits = inits.null,
+                        model.file = br_w_as_model,
+                        n.iter = ni,
+                        n.burnin = nb,
+                        n.thin = nt,
+                        n.chains = nc) 
+
+BR_w_SPP_output
+
+write.csv(BR_w_SPP_output$BUGSoutput$summary, file = "BR_P_SPP_SSPop_output.csv")
+
+# autocorr.plot(wmdspecific_wmdsurv_output,ask=F,auto.layout = T)
+# 
+# plot(as.mcmc(BR_w_SPP_output))
+# 
+# traceplot(BR_w_SPP_output)
