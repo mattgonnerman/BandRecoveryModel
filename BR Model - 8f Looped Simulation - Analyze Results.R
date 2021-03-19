393db15c1fd9cb2ec0a79e0fbe0eb67b68e09703
Matt Gonnerman <matthew.gonnerman@maine.edu>
2021-03-19 11:49:48 -0400

    Started Coding for Bias Assessment

--- BR Model - 8f Looped Simulation - Analyze Results.R
+++ BR Model - 8f Looped Simulation - Analyze Results.R
@@ -29,10 +29,12 @@ SimInfo.NIndWSR <- SimInfo.raw %>%
   filter(Name == "NInd WSR")%>%
   rename(NInd_WSR = Value) %>% 
   select(-Name)
-SimInfo.totaldata <- SimInfo.raw[1:5,] %>%
+SimInfo.totaldata <- SimInfo.raw %>%
+  filter(Name %in% c("YearsBR", "TotNIndBR", "YearsTotHarv", "YearsWSR", "TotNIndBR")) %>%
   select(-WMD) %>%
   pivot_wider(names_from = Name, values_from = Value)
 
+#Individual WMD Bias
 N.bias <- merge(N.bias.long, SimInfo.NSitesBR, by = c("WMD","Trial"))
 N.bias <- merge(N.bias, SimInfo.NSitesWSR, by = c("WMD","Trial"))
 N.bias <- merge(N.bias, SimInfo.NIndBR, by = c("WMD","Trial"))
@@ -47,3 +49,40 @@ ggplot(N.bias, aes(x = NInd_BR, y = value)) +
   geom_point(aes(color = Age))
 ggplot(N.bias, aes(x = NInd_WSR, y = value)) +
   geom_point(aes(color = Age))
+
+NBias.fullmodel <- lm(value ~ NSites_BR + NSites_WSR + NInd_BR + NInd_WSR, N.bias)
+summary(NBias.fullmodel)
+
+#Summarize WMDs
+N.bias.simple <- N.bias.long %>%
+  group_by(Trial, WMD, Age) %>%
+  summarize(Mean = mean(value, na.rm = T), StdDev= sd(value, na.rm = T))
+N.bias.simple <- merge(N.bias.simple, SimInfo.NSitesBR, by = c("WMD","Trial"))
+N.bias.simple <- merge(N.bias.simple, SimInfo.NSitesWSR, by = c("WMD","Trial"))
+N.bias.simple <- merge(N.bias.simple, SimInfo.NIndBR, by = c("WMD","Trial"))
+N.bias.simple <- merge(N.bias.simple, SimInfo.NIndWSR, by = c("WMD","Trial"))
+N.bias.simple <- merge(N.bias.simple, SimInfo.totaldata, by = c("Trial"))
+
+ggplot(N.bias.simple, aes(x = NSites_BR, y = Mean)) +
+  geom_point(aes(color = Age))
+ggplot(N.bias.simple, aes(x = NSites_WSR, y = Mean)) +
+  geom_point(aes(color = Age))
+ggplot(N.bias.simple, aes(x = NInd_BR, y = Mean)) +
+  geom_point(aes(color = Age))
+ggplot(N.bias.simple, aes(x = NInd_WSR, y = Mean)) +
+  geom_point(aes(color = Age))
+
+mean.NBias.fullmodel <- lm(Mean ~ NSites_BR + NSites_WSR + NInd_BR + NInd_WSR, N.bias.simple)
+summary(mean.NBias.fullmodel)
+
+ggplot(N.bias.simple, aes(x = NSites_BR, y = StdDev)) +
+  geom_point(aes(color = Age))
+ggplot(N.bias.simple, aes(x = NSites_WSR, y = StdDev)) +
+  geom_point(aes(color = Age))
+ggplot(N.bias.simple, aes(x = NInd_BR, y = StdDev)) +
+  geom_point(aes(color = Age))
+ggplot(N.bias.simple, aes(x = NInd_WSR, y = StdDev)) +
+  geom_point(aes(color = Age))
+
+sd.NBias.fullmodel <- lm(StdDev ~ NSites_BR + NSites_WSR + NInd_BR + NInd_WSR, N.bias.simple)
+summary(sd.NBias.fullmodel)
