684e6d2227aa8d812d3e69a0e54094cdf8919658
Matthew Gonnerman <matthew.gonnerman@maine.edu>
2021-03-29 10:24:24 -0400

    Updated graph code

--- BR Model - 8a Looped Simulation - Sample Size Bias.R
+++ BR Model - 8a Looped Simulation - Sample Size Bias.R
@@ -11,11 +11,11 @@ nb <- 5000 #burn in period
 nc <- 8 #number of cores
 
 #How Many Simulations would you like to run?
-looprun1 <- 150
+looprun1 <- 50
 
 #Simplified loop, broken into multiple scripts
 #Saves individual loop outputs to their own files
-for(looprun in 18:looprun1){
+for(looprun in 30:looprun1){
   
   print(paste("Run", looprun, "Start Time:", Sys.time(), sep = " "))
   
@@ -44,6 +44,12 @@ for(looprun in 18:looprun1){
   print(paste("Run", looprun, "End Time:", Sys.time(), sep = " "))
 }
 
+#If failure/computer freeze/etc, load the most recent info using code below and start loop at appropriate number
+master.bias.hr <- read.csv("Model Bias Comparison/SampleSize/Master HR Bias.csv")
+master.bias.wsr <- read.csv("Model Bias Comparison/SampleSize/Master WSR Bias.csv")
+master.bias.N <- read.csv("Model Bias Comparison/SampleSize/Master N Bias.csv", check.names=FALSE)
+master.bias.R <- read.csv("Model Bias Comparison/SampleSize/Master R Bias.csv", check.names=FALSE)
+mastersiteinfo <- read.csv("Model Bias Comparison/SampleSize/MasterSimInfo.csv")
 
 #Analyze Results
 source(file = "BR Model - 8f Looped Simulation - Analyze Results.R")
--- BR Model - 8f Looped Simulation - Analyze Results.R
+++ BR Model - 8f Looped Simulation - Analyze Results.R
@@ -3,15 +3,16 @@ require(dplyr)
 require(tidyr)
 
 # Load Files
-N.bias.raw <- read.csv("Model Bias Comparison/SampleSize/Master N Bias.csv")
+N.bias.raw <- read.csv("Model Bias Comparison/SampleSize/Master N Bias.csv", check.names=FALSE)
 HR.bias.raw <- read.csv("Model Bias Comparison/SampleSize/Master HR Bias.csv")
 WSR.bias.raw <- read.csv("Model Bias Comparison/SampleSize/Master WSR Bias.csv")
-R.bias.raw <- read.csv("Model Bias Comparison/SampleSize/Master R Bias.csv")
+R.bias.raw <- read.csv("Model Bias Comparison/SampleSize/Master R Bias.csv", check.names=FALSE)
 SimInfo.raw <- read.csv("Model Bias Comparison/SampleSize/MasterSimInfo.csv")
 
 N.bias.long <- N.bias.raw %>%
   pivot_longer(cols = 4:ncol(N.bias.raw)) %>%
-  mutate(Year = as.integer(sub(".", "", name))) %>% select(-name)
+  rename(Year = name) %>%
+  filter(!is.na(value))
 
 SimInfo.NSitesBR <- SimInfo.raw %>%
   filter(Name == "NSites BR") %>%
@@ -39,50 +40,125 @@ N.bias <- merge(N.bias.long, SimInfo.NSitesBR, by = c("WMD","Trial"))
 N.bias <- merge(N.bias, SimInfo.NSitesWSR, by = c("WMD","Trial"))
 N.bias <- merge(N.bias, SimInfo.NIndBR, by = c("WMD","Trial"))
 N.bias <- merge(N.bias, SimInfo.NIndWSR, by = c("WMD","Trial"))
-N.bias <- merge(N.bias, SimInfo.totaldata, by = c("Trial"))
+N.bias <- merge(N.bias, SimInfo.totaldata, by = c("Trial")) %>%
+  group_by(Age) %>%
+  mutate(NInd_BR_bin = as.numeric(cut(NInd_BR, breaks=(10*(1:ceiling(max(N.bias$NInd_BR)/10)-1)),
+                           labels=10*(10*(2:ceiling(max(N.bias$NInd_BR)/10)-1))))) %>%
+  mutate(NInd_WSR_bin = as.numeric(cut(NInd_WSR, breaks=(10*(1:ceiling(max(N.bias$NInd_WSR)/10)-1)),
+                           labels=(10*(2:ceiling(max(N.bias$NInd_WSR)/10)-1))))) %>%
+  mutate(NInd_BR_bin = ifelse(is.na(NInd_BR_bin), 0, 10*NInd_BR_bin)) %>%
+  mutate(NInd_WSR_bin = ifelse(is.na(NInd_WSR_bin), 0, 10*NInd_WSR_bin))
 
-ggplot(N.bias, aes(x = NSites_BR, y = value)) +
-  geom_point(aes(color = Age))
-ggplot(N.bias, aes(x = NSites_WSR, y = value)) +
-  geom_point(aes(color = Age))
-ggplot(N.bias, aes(x = NInd_BR, y = value)) +
-  geom_point(aes(color = Age))
-ggplot(N.bias, aes(x = NInd_WSR, y = value)) +
-  geom_point(aes(color = Age))
+#Density Plots
+ggplot(N.bias, aes(x = value, group = Age)) +
+  geom_density(aes(fill = Age), alpha = .7) +
+  xlim(-500, 400) +
+  labs(title = "Bias in WMD Specific Abundance",
+       subtitle = "Juvenile vs Adult") +
+  xlab("Difference From Real Value")
+ggsave("Model Bias Comparison/SampleSize/Graphs/Nbias_Age.jpg")
 
-NBias.fullmodel <- lm(value ~ NSites_BR + NSites_WSR + NInd_BR + NInd_WSR, N.bias)
-summary(NBias.fullmodel)
 
-#Summarize WMDs
-N.bias.simple <- N.bias.long %>%
-  group_by(Trial, WMD, Age) %>%
-  summarize(Mean = mean(value, na.rm = T), StdDev= sd(value, na.rm = T))
-N.bias.simple <- merge(N.bias.simple, SimInfo.NSitesBR, by = c("WMD","Trial"))
-N.bias.simple <- merge(N.bias.simple, SimInfo.NSitesWSR, by = c("WMD","Trial"))
-N.bias.simple <- merge(N.bias.simple, SimInfo.NIndBR, by = c("WMD","Trial"))
-N.bias.simple <- merge(N.bias.simple, SimInfo.NIndWSR, by = c("WMD","Trial"))
-N.bias.simple <- merge(N.bias.simple, SimInfo.totaldata, by = c("Trial"))
+require(RColorBrewer)
+nb.cols <- length(unique(N.bias$NSites_BR))
+mycolors <- colorRampPalette(brewer.pal(8, "Spectral"))(nb.cols)
+ggplot(N.bias[N.bias$Age=="A",], aes(x = value, group = as.factor(NSites_BR))) +
+  geom_density(aes(fill = as.factor(NSites_BR)), alpha = .7) +
+  xlim(-1000, 300) +
+  scale_fill_manual(values = mycolors) +
+  labs(title = "Bias in WMD Specific Abundance",
+       subtitle = "Number BR Sites in WMD | Adult") +
+  xlab("Difference From Real Value") + 
+  guides(fill=guide_legend(title="# BR Sites\nIn Region"))
+ggsave("Model Bias Comparison/SampleSize/Graphs/Nbias_NSitesBR_A.jpg")
 
-ggplot(N.bias.simple, aes(x = NSites_BR, y = Mean)) +
-  geom_point(aes(color = Age))
-ggplot(N.bias.simple, aes(x = NSites_WSR, y = Mean)) +
-  geom_point(aes(color = Age))
-ggplot(N.bias.simple, aes(x = NInd_BR, y = Mean)) +
-  geom_point(aes(color = Age))
-ggplot(N.bias.simple, aes(x = NInd_WSR, y = Mean)) +
-  geom_point(aes(color = Age))
+ggplot(N.bias[N.bias$Age=="J",], aes(x = value, group = as.factor(NSites_BR))) +
+  geom_density(aes(fill = as.factor(NSites_BR)), alpha = .7) +
+  xlim(-1000, 300)  +
+  scale_fill_manual(values = mycolors) +
+  labs(title = "Bias in WMD Specific Abundance",
+       subtitle = "Number BR Sites in WMD | Juv") +
+  xlab("Difference From Real Value") + 
+  guides(fill=guide_legend(title="# BR Sites\nIn Region"))
+ggsave("Model Bias Comparison/SampleSize/Graphs/Nbias_NSitesBR_J.jpg")
 
-mean.NBias.fullmodel <- lm(Mean ~ NSites_BR + NSites_WSR + NInd_BR + NInd_WSR, N.bias.simple)
-summary(mean.NBias.fullmodel)
+nb.cols <- length(unique(N.bias$NSites_WSR))
+mycolors <- colorRampPalette(brewer.pal(8, "Spectral"))(nb.cols)
+ggplot(N.bias[N.bias$Age=="A",], aes(x = value, group = as.factor(NSites_WSR))) +
+  geom_density(aes(fill = as.factor(NSites_WSR)), alpha = .7) +
+  xlim(-1000, 300) +
+  scale_fill_manual(values = mycolors) +
+  labs(title = "Bias in WMD Specific Abundance",
+       subtitle = "Number WSR Sites in WMD | Adult") +
+  xlab("Difference From Real Value") + 
+  guides(fill=guide_legend(title="# WSR Sites\nIn Region"))
+ggsave("Model Bias Comparison/SampleSize/Graphs/Nbias_NSitesWSR_A.jpg")
 
-ggplot(N.bias.simple, aes(x = NSites_BR, y = StdDev)) +
-  geom_point(aes(color = Age))
-ggplot(N.bias.simple, aes(x = NSites_WSR, y = StdDev)) +
-  geom_point(aes(color = Age))
-ggplot(N.bias.simple, aes(x = NInd_BR, y = StdDev)) +
-  geom_point(aes(color = Age))
-ggplot(N.bias.simple, aes(x = NInd_WSR, y = StdDev)) +
-  geom_point(aes(color = Age))
+ggplot(N.bias[N.bias$Age=="J",], aes(x = value, group = as.factor(NSites_WSR))) +
+  geom_density(aes(fill = as.factor(NSites_WSR)), alpha = .7) +
+  xlim(-1000, 300)   +
+  scale_fill_manual(values = mycolors) +
+  labs(title = "Bias in WMD Specific Abundance",
+       subtitle = "Number WSR Sites in WMD | Juv") +
+  xlab("Difference From Real Value") + 
+  guides(fill=guide_legend(title="# WSR Sites\nIn Region"))
+ggsave("Model Bias Comparison/SampleSize/Graphs/Nbias_NSitesWSR_J.jpg")
 
-sd.NBias.fullmodel <- lm(StdDev ~ NSites_BR + NSites_WSR + NInd_BR + NInd_WSR, N.bias.simple)
-summary(sd.NBias.fullmodel)
+nb.cols <- length(unique(N.bias$NInd_BR_bin))
+mycolors <- colorRampPalette(brewer.pal(8, "Spectral"))(nb.cols)
+ggplot(N.bias[N.bias$Age=="A",], aes(x = value, group = as.factor(NInd_BR_bin))) +
+  geom_density(aes(fill = as.factor(NInd_BR_bin)), alpha = .7) +
+  xlim(-1000, 300) +
+  scale_fill_manual(values = mycolors) +
+  labs(title = "Bias in WMD Specific Abundance",
+       subtitle = "Number BR Individuals in WMD (Binned by 10s) | Adult") +
+  xlab("Difference From Real Value") + 
+  guides(fill=guide_legend(title="# BR Individuals\nIn Region"))
+ggsave("Model Bias Comparison/SampleSize/Graphs/Nbias_NIndBR_A.jpg")
+
+ggplot(N.bias[N.bias$Age=="J",], aes(x = value, group = as.factor(NInd_BR_bin))) +
+  geom_density(aes(fill = as.factor(NInd_BR_bin)), alpha = .7) +
+  xlim(-1000, 300) +
+  scale_fill_manual(values = mycolors) +
+  labs(title = "Bias in WMD Specific Abundance",
+       subtitle = "Number BR Individuals in WMD (Binned by 10s) | Juv") +
+  xlab("Difference From Real Value") + 
+  guides(fill=guide_legend(title="# BR Indiviudals\nIn Region"))
+ggsave("Model Bias Comparison/SampleSize/Graphs/Nbias_NIndBR_J.jpg")
+
+nb.cols <- length(unique(N.bias$NInd_WSR_bin))
+mycolors <- colorRampPalette(brewer.pal(8, "Spectral"))(nb.cols)
+ggplot(N.bias[N.bias$Age=="A",], aes(x = value, group = as.factor(NInd_WSR_bin))) +
+  geom_density(aes(fill = as.factor(NInd_WSR_bin)), alpha = .7) +
+  xlim(-1000, 300) +
+  scale_fill_manual(values = mycolors) +
+  labs(title = "Bias in WMD Specific Abundance",
+       subtitle = "Number WSR Individuals in WMD (Binned by 10s) | Adult") +
+  xlab("Difference From Real Value") + 
+  guides(fill=guide_legend(title="# WSR Individuals\nIn Region"))
+ggsave("Model Bias Comparison/SampleSize/Graphs/Nbias_NIndWSR_A.jpg")
+
+ggplot(N.bias[N.bias$Age=="J",], aes(x = value, group = as.factor(NInd_WSR_bin))) +
+  geom_density(aes(fill = as.factor(NInd_WSR_bin)), alpha = .7) +
+  xlim(-1000, 300)  +
+  scale_fill_manual(values = mycolors) +
+  labs(title = "Bias in WMD Specific Abundance",
+       subtitle = "Number WSR Individuals in WMD (Binned by 10s) | Juv") +
+  xlab("Difference From Real Value") + 
+  guides(fill=guide_legend(title="# WSR Individuals\nIn Region"))
+ggsave("Model Bias Comparison/SampleSize/Graphs/Nbias_NIndWSR_J.jpg")
+
+
+
+
+#Point Graphs
+ggplot(N.bias[N.bias$Age=="A",], aes(x = as.factor(NSites_BR), y = value)) +
+  geom_point(aes(color = Age))
+ggplot(N.bias[N.bias$Age=="J",], aes(x = as.factor(NSites_BR), y = value)) +
+  geom_violin(aes(color = Age))
+ggplot(N.bias[N.bias$Age=="A",], aes(x = NSites_WSR, y = value)) +
+  geom_point(aes(color = Age))
+ggplot(N.bias[N.bias$Age=="A",], aes(x = NInd_BR, y = value)) +
+  geom_point(aes(color = Age))
+ggplot(N.bias[N.bias$Age=="A",], aes(x = NInd_WSR, y = value)) +
+  geom_point(aes(color = Age))
