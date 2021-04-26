50d6ae7ea2a9b0a3617b534a1f74bc9bd9951223
Matthew Gonnerman <matthew.gonnerman@maine.edu>
2021-04-26 11:48:52 -0400

    Introduced Cap on population growth in simulated data

--- BR Final Sim - 0 Run Code.R
+++ BR Final Sim - 0 Run Code.R
@@ -14,16 +14,16 @@ nc <- detectCores()/2 #number of cores
 ### Set Variable Parameters 
 ## HR Gaussian Process
 # Magnitude of variation, value that variogram levels out at 
-psill.hr <- 0.1 #c(0.001, 0.01, 0.1)
+psill.hr <- 0.01 #c(0.001, 0.01, 0.1)
 # Maximal distance of autocorrelation, where variogram levels out
 hr.sc <- 0.005 #c(0.001, 0.005, 0.01)
 # Small-scale variations
 nugget.hr <- 7 #c(2, 7, 15)
 
 #Which Trial area you running (e.g. "LowNugget", "MedPSill", "HighRange")
-trialname <- "HighPSill"
+trialname <- "MedAll"
 
-for(looprun in 11:100){
+for(looprun in 1:25){
   print(paste("Run", looprun, "Start Time:", Sys.time(), sep = " "))
   
   #Generate Simulated Dataset
