736414f295eb2885dd80530bef238199abd6a4c8
Matthew Gonnerman <matthew.gonnerman@maine.edu>
2022-03-25 11:03:17 -0400

    Link funciton correction in original model

--- BR Final Sim - 0 Run Code.R
+++ BR Final Sim - 0 Run Code.R
@@ -12,16 +12,16 @@ require(ggplot2)
 ni <- 10000 #number of iterations
 nt <- 8 #thinning
 nb <- 5000 #burn in period
-nc <- detectCores() - 1 #number of cores
+nc <- detectCores()/2 #number of cores
 
 ### Set Variable Parameters 
 ## HR Gaussian Process
 # Magnitude of variation, value that variogram levels out at 
-psill.hr <- 0.001 #c(0.001, 0.01, 0.1)
+psill.hr <- 0.01 #c(0.001, 0.01, 0.1)
 # Maximal distance of autocorrelation, where variogram levels out
 hr.sc <- 7 #c(2, 7, 15)
 # Small-scale variations
-nugget.hr <- 0.005 #c(0.001, 0.005, 0.01)
+nugget.hr <- 0.001 #c(0.001, 0.005, 0.01)
 
 #Which Trial area you running (e.g. "LowNugget", "MedPSill", "HighRange")
 #Add "_noSPP" for secondary run without accounting for spatial variation
@@ -29,9 +29,9 @@ nugget.hr <- 0.005 #c(0.001, 0.005, 0.01)
 # Current: low 
 # Completed: 
 
-trialname <- "LowPSill_noSPP"
+trialname <- "LowNugget"
 
-for(looprun in 2:100){
+for(looprun in 1:100){
   print(paste("Run", looprun, "Start Time:", Sys.time(), sep = " "))
   
   #Generate Simulated Dataset
@@ -51,8 +51,8 @@ for(looprun in 2:100){
                          looprun, " - RawModel Estimates.csv", sep = ""))
   
   #Summarize Model Results
-  # source(file = "BR Final Sim - 5 Summarize Model Results.R")
-  source(file = "BR Final Sim - 5c Summarize Model Results No SPP.R")
+  source(file = "BR Final Sim - 5 Summarize Model Results.R")
+  # source(file = "BR Final Sim - 5c Summarize Model Results No SPP.R")
   
   #Save Simulated Data Information
   source(file = "BR Final Sim - 6 Dataset Info.R")
@@ -63,13 +63,13 @@ for(looprun in 2:100){
   print(paste("Run", looprun, "End Time:", Sys.time(), sep = " "))
 }
 
-#If failure/computer freeze/etc, load the most recent info using code below and start loop at appropriate number
-master.bias.hr <- read.csv(paste("E:/Maine Drive/Analysis/Band Recovery/FinalSim/", trialname, " - Master HR Bias.csv", sep = ""))
-master.bias.wsr <- read.csv(paste("E:/Maine Drive/Analysis/Band Recovery/FinalSim/", trialname, " - Master WSR Bias.csv", sep = ""))
-master.bias.N <- read.csv(paste("E:/Maine Drive/Analysis/Band Recovery/FinalSim/", trialname, " - Master N Bias.csv", sep = ""), check.names=FALSE)
-master.bias.R <- read.csv(paste("E:/Maine Drive/Analysis/Band Recovery/FinalSim/", trialname, " - Master R Bias.csv", sep = ""), check.names=FALSE)
-mastersiteinfo <- read.csv(paste("E:/Maine Drive/Analysis/Band Recovery/FinalSim/", trialname, " - MasterSimInfo.csv", sep = ""))
-max(mastersiteinfo$Trial)
+# ### If failure/computer freeze/etc, load the most recent info using code below and start loop at appropriate number
+# master.bias.hr <- read.csv(paste("E:/Maine Drive/Analysis/Band Recovery/FinalSim/", trialname, " - Master HR Bias.csv", sep = ""))
+# master.bias.wsr <- read.csv(paste("E:/Maine Drive/Analysis/Band Recovery/FinalSim/", trialname, " - Master WSR Bias.csv", sep = ""))
+# master.bias.N <- read.csv(paste("E:/Maine Drive/Analysis/Band Recovery/FinalSim/", trialname, " - Master N Bias.csv", sep = ""), check.names=FALSE)
+# master.bias.R <- read.csv(paste("E:/Maine Drive/Analysis/Band Recovery/FinalSim/", trialname, " - Master R Bias.csv", sep = ""), check.names=FALSE)
+# mastersiteinfo <- read.csv(paste("E:/Maine Drive/Analysis/Band Recovery/FinalSim/", trialname, " - MasterSimInfo.csv", sep = ""))
+# max(mastersiteinfo$Trial)
 
 
 
